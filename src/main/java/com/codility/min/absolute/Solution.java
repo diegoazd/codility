b309e81c0b357e540ad06ab974c5a28cb087328e
Diego Armando Zarate Diaz <diegorlz@MacBook-Pro-de-Diego-2.local>
2014-08-28 19:10:42 -0500

    refactor

--- src/main/java/com/codility/min/absolute/Solution.java
+++ src/main/java/com/codility/min/absolute/Solution.java
@@ -1,14 +1,32 @@
 package com.codility.min.absolute;
 
-class Solution {
+
+class Solution { 
+  
+  boolean isZero(int number){
+    boolean verify = number == 0 ? true: false;
+    return verify;
+  }
+
+  boolean findZeroInList(int[] A){
+    for(int i = 0; i < A .length; i++){
+      if(isZero(A[i])){
+        return true;
+      }
+    }
+    return false;
+  }
 
   int solution(int[] A){
    int size = A.length;
    int minAbsSlice = 10000;
+   
+   if(findZeroInList(A)){
+     return 0;
+   }
+
+
    for(int i=0; i < size; i++){
-     if(A[i] == 0){
-       return 0;
-     }
      int limit = i;
      int looping = i;
      boolean flag = true;   
@@ -20,7 +38,7 @@ class Solution {
          if(Math.abs(sumAbsSlice) < minAbsSlice){
            minAbsSlice = Math.abs(sumAbsSlice);
            System.out.println(minAbsSlice);
-           if(minAbsSlice == 0){
+           if(isZero(minAbsSlice)){
              return minAbsSlice;
             }
          }
--- src/test/java/com/codility/min/absolute/SolutionTest.java
+++ src/test/java/com/codility/min/absolute/SolutionTest.java
@@ -2,7 +2,7 @@ package com.codility.min.absolute;
 
 import org.junit.runner.RunWith;
 import org.junit.Test;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 public class SolutionTest {
 
@@ -12,5 +12,37 @@ public class SolutionTest {
     int [] A = {2,-4,6,-3,9};
     int res = solution.solution(A);    
     org.junit.Assert.assertEquals(res, 1);
+  }
+
+  @Test
+  public void shouldPassWithOneResult(){
+    Solution solution = new Solution();
+    int[] A = {2};
+    int res = solution.solution(A);
+    assertEquals(res,2);
+  }
+
+  @Test
+  public void shouldPassWithWorstCaseIsZero(){
+  int[] A = new int[100000];
+  for(int i = 0; i < A.length; i++){
+    A[i] = 2;
+  }
+  A[99999] = 0;
+  Solution solution = new Solution();
+  int res = solution.solution(A);
+  assertEquals(res,0);
+  }
+  
+  @Test
+  public void shouldPassWithWorstCase(){
+  int[] A = new int[100000];
+  for(int i = 0; i < A.length; i++){
+    A[i] = 2;
+  }
+  A[99999] = 1;
+  Solution solution = new Solution();
+  int res = solution.solution(A);
+  assertEquals(res,0);
   }
 }
